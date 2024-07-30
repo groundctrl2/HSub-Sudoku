9eb95e94831b02c6ff914d2948f9db530505155d
Tommy Collier <98130531+groundctrl2@users.noreply.github.com>
2024-07-29 18:13:18 -0600

    Index/Position based DFS
    - Graph is now index/position based
    - Connected Components based on graph

--- Assets/Scripts/HSubGraph.cs
+++ Assets/Scripts/HSubGraph.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,14 @@ using UnityEngine.Rendering.Universal.Internal;
 
 public class HSubGraph
 {
-    // Zero-indexed adjacency lists representing each value
-    public List<(int value1, int value2, int value3, int value4)>[] adjValues;
-    public List<int>[] adjIndices;
+    private List<int>[] noteGroup;
+    private int[][] valueOccurrences; // Jagged to allow arrays of different sizes
 
-    // Connected Components variables
+    // Graph variables
+    public List<int>[] adj; // Zero-indexed adjacency list representing each index
+    public List<int>[,] adjValues; // adjacency list companion, stores the values shared between vertices
+
+    // Connected Components (CC) variables
     private bool[] marked;
     private int[] id;
     private int[] size;
@@ -18,28 +22,21 @@ public class HSubGraph
     // Constructor
     public HSubGraph(List<int>[] noteGroup)
     {
+        this.noteGroup = noteGroup;
+        valueOccurrences = HSubRules.GetValueOccurrences(noteGroup);
+
         // Graph construction
-        adjValues = new List<(int value1, int value2, int value3, int value4)>[9];
-        adjIndices = new List<int>[9];
+        adj = new List<int>[9];
+        adjValues = new List<int>[9, 9];
 
-        // Fill adj values and indices
-        for (int v = 0; v < 9; v++)
+        for (int i = 0; i < 9; i++)
         {
-            adjValues[v] = new List<(int value1, int value2, int value3, int value4)>();
-            adjIndices[v] = new List<int>();
+            adj[i] = new List<int>();
+            for (int j = 0; j < 9; j++)
+                adjValues[i, j] = new List<int>();
         }
 
-        // Add edges to adj
-        for (int v = 0; v < 9; v++)
-        {
-            if (noteGroup[v].Count <= 4)
-            {
-                int[] values = new int[4];
-                for (int i = 0; i < noteGroup[v].Count; i++)
-                    values[i] = noteGroup[v][i];
-                AddEdge(v, (values[0], values[1], values[2], values[3]));
-            }
-        }
+        AddEdges();
 
         // Connected Components (CC) Construction
         marked = new bool[9];
@@ -55,57 +52,44 @@ public class HSubGraph
         }
     }
 
-    // Add the path to each element of the path as an edge in adj values and indices
-    private void AddEdge(int index, (int value1, int value2, int value3, int value4) path)
+    // For every value shared between 2+ index vertices, add an edge
+    private void AddEdges()
     {
-        int[] values = { path.value1, path.value2, path.value3, path.value4 };
-        for (int i = 0; i < 4; i++)
+        for (int value = 1; value < 10; value++)
         {
-            if (values[i] != 0)
-            {
-                adjValues[values[i] - 1].Add(path);
-                adjIndices[values[i] - 1].Add(index);
-            }
+            // Add each edge once
+            var occurrences = valueOccurrences[value - 1];
+            for (int i = 0; i < occurrences.Length; i++)
+                for (int j = 0; j < occurrences.Length; j++)
+                    if (i < j)
+                    {
+                        int v = occurrences[i];
+                        int w = occurrences[j];
+                        if (noteGroup[v].Count <= 4 && noteGroup[w].Count <= 4)
+                        {
+                            // Add w to v
+                            if (!adj[v].Contains(w))
+                                adj[v].Add(w);
+                            adjValues[v, w].Add(value);
+
+                            // Add v to w
+                            if (!adj[w].Contains(v))
+                                adj[w].Add(v);
+                            adjValues[w, v].Add(value);
+                        }
+                    }
         }
     }
 
-    // Depth first search for Connected Component (CC) construction
-    private void DepthFirstSearch(int valueIndex)
+    // Depth first search for CC construction
+    private void DepthFirstSearch(int v)
     {
-        marked[valueIndex] = true;
-        id[valueIndex] = count;
+        marked[v] = true;
+        id[v] = count;
         size[count]++;
 
-        foreach ((int value1, int value2, int value3, int value4) adj in adjValues[valueIndex])
-        {
-            int[] values = { adj.value1, adj.value2, adj.value3, adj.value4 };
-            for (int i = 0; i < 4; i++)
-                if (values[i] != 0 && !marked[values[i] - 1])
-                    DepthFirstSearch(values[i] - 1);
-        }
-    }
-
-    // Return the component id of the CC containing the given value/vertex
-    public int GetID(int value)
-    {
-        return id[value - 1];
-    }
-
-    // Return the size of the CC containing the given value/vertex
-    public int GetSize(int value)
-    {
-        return size[id[value - 1]];
-    }
-
-    // Return the amount of Connected Components in the graph
-    public int GetCount()
-    {
-        return count;
-    }
-
-    // Returns whether the given 2 values/vertices are connected in the same CC
-    public bool AreConnected(int value1, int value2)
-    {
-        return id[value1 - 1] == id[value2 - 1];
+        foreach (int w in adj[v])
+            if (!marked[w])
+                DepthFirstSearch(w);
     }
 }
--- Assets/Scripts/HSubRules.cs
+++ Assets/Scripts/HSubRules.cs
@@ -4,10 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Data;
 using UnityEngine;
+using Unity.Burst.Intrinsics;
+using Unity.VisualScripting;
 
 public class HSubRules
 {
     public List<int>[] hsubGrid = new List<int>[81];
+
+    // Found hidden subsets
+    private List<(int index, int value)> singles;
+    private List<(int index1, int index2, int value1, int value2)> doubles;
+    private List<(int index1, int index2, int index3, int value1, int value2, int value3)> triples;
+    private List<(int index1, int index2, int index3, int index4, int value1, int value2, int value3, int value4)> quadruples;
 
     // Constructor to initialize with the current 1D notes grid state
     public HSubRules(bool[][] currentNotes)
@@ -46,7 +54,7 @@ public class HSubRules
     }
 
     // Takes a note group (row/col/subgrid) and returns a zero-indexed array containing how many times each value occurs
-    private int[] CountDigitOccurrences(List<int>[] noteGroup)
+    private static int[] CountDigitOccurrences(List<int>[] noteGroup)
     {
         int[] valueOccurrences = new int[9];
         for (int i = 0; i < 9; i++)
@@ -58,7 +66,7 @@ public class HSubRules
     }
 
     // Get the index/indices of a given value in a given note group (row/col/subgrid) based on the amount of occurrences to find
-    private int[] FindOccurrenceIndices(List<int>[] noteGroup, int value, int occurrenceCount)
+    private static int[] FindOccurrenceIndices(List<int>[] noteGroup, int value, int occurrenceCount)
     {
         int[] occurrenceIndices = new int[occurrenceCount];
 
@@ -75,5 +83,30 @@ public class HSubRules
         }
 
         return occurrenceIndices;
+    }
+
+    // Get the occurrence indices of each value in a note group
+    public static int[][] GetValueOccurrences(List<int>[] noteGroup)
+    {
+        var valueOccurrences = new int[9][]; // Jagged to allow arrays of different sizes
+        var occurrenceCounts = CountDigitOccurrences(noteGroup);
+
+        for (int i = 0; i < 9; i++)
+            valueOccurrences[i] = FindOccurrenceIndices(noteGroup, i + 1, occurrenceCounts[i]);
+
+        return valueOccurrences;
+    }
+
+    // Get hashset containing all unique values from given lists
+    public static HashSet<int> GetUniqueValues(List<int> list1, List<int> list2)
+    {
+        var hashSet = new HashSet<int>();
+
+        foreach (int value in list1)
+            hashSet.Add(value);
+        foreach (int value in list2)
+            hashSet.Add(value);
+
+        return hashSet;
     }
 }
